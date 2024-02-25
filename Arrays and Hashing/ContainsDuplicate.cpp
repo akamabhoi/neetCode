105ca7272e4659e3111cfe305cf6a81f5b8da900
SHUBHRANSHU SHEKHAR BHOI <bhoi.shubhranshu78@gmail.com>
2024-02-25 23:55:10 +0530

    Create ContainsDuplicate.cpp

---
+++ Arrays and Hashing/ContainsDuplicate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    // Function to check if the given vector contains any duplicate elements
+    bool containsDuplicate(vector<int>& nums) {
+        // Create an unordered map to store the frequency of each number
+        unordered_map<int, int> mp;
+
+        // Iterate through the vector
+        for(int num: nums){
+            // If the number is already in the map, a duplicate is found
+            if(mp[num] >= 1){
+                return true;
+            }
+
+            // Increment the frequency count of the current number in the map
+            mp[num]++;
+        }
+
+        // No duplicates found, return false
+        return false;
+    }
+};
