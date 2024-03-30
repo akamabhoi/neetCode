77102c2f53078b4ea71aa0241fc65ca09104c272
SHUBHRANSHU SHEKHAR BHOI <bhoi.shubhranshu78@gmail.com>
2024-03-30 14:04:37 +0530

    Update ReorderList.cpp

--- Linked List/ReorderList.cpp
+++ Linked List/ReorderList.cpp
@@ -1,48 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
 class Solution
 {
 public:
     // Function to reorder a linked list
     void reorderList(ListNode *head)
     {
-        // Base case: linked list is empty
-        if (!head)
-            return;
-
-        // Finding the middle with the help of two-pointer approach
-        ListNode *tmp = head, *half = head, *prev = NULL;
-        while (tmp->next && tmp->next->next)
+        // Initialize two pointers slow and fast to find the middle of the list
+        ListNode *slow = head, *fast = head;
+        while (fast and fast->next)
         {
-            tmp = tmp->next->next;
-            half = half->next;
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        // Adding one step in case of lists with even length
-        if (tmp->next)
-            half = half->next;
-
-        // Now reverse the second half
-        while (half)
+        // Reverse the second half of the list
+        ListNode *prev = nullptr, *curr = slow, *nex = nullptr;
+        while (curr)
         {
-            tmp = half->next;
-            half->next = prev;
-            prev = half;
-            half = tmp;
-        }
-        half = prev;
-
-        // After reversing the second half, let's merge both the halves
-        while (head && half)
-        {
-            tmp = head->next;
-            prev = half->next;
-            head->next = half;
-            half->next = tmp;
-            head = tmp;
-            half = prev;
+            nex = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nex;
         }
 
-        // Base case: closing when we had even-length arrays
-        if (head && head->next)
-            head->next->next = NULL;
+        // Merge the first half and the reversed second half of the list
+        ListNode *head1 = head, *head2 = prev;
+        while (head2->next)
+        {
+            ListNode *temp = head1->next;
+            head1->next = head2;
+            head1 = temp;
+            temp = head2->next;
+            head2->next = head1;
+            head2 = temp;
+        }
     }
 };
